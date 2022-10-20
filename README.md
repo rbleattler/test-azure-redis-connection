1cef0e0cd6927aceeacbd16448acd8589d66022a
Rob Bleattler <Robert.Bleattler@freedompay.com>
2022-10-20 18:15:06 -0400

    update arg processing. Update readme

--- README.md
+++ README.md
@@ -4,21 +4,43 @@ Console app to test the connection to an Azure Redis
 
 ## Steps
 
->[!info] Clone repository
+> Clone repository
+
 ```powershell
 git clone https://github.com/rbleattler/test-azure-redis-connection.git; Set-Location .\test-azure-redis-connection
 ```
 
-### Build and execute the application
+## How to Use This Tool
 
-Run the tool with the connection details 
-TODO: Update to support secure strings
+Run the tool with the connection details. All examples are in PowerShell. For PowerShell versions < 5.1 precede the exe call with a period. *(example: `. cachedebugger`)*
+
+> Using a secure string as the Primary Key input with the default port (6380)
+
 ```powershell
 cachedebugger.exe my-redis-resourcename $(Read-Host -AsSecureString | ConvertFrom-SecureString -AsPlainText)
-
-cachedebugger.exe my-redis-resourcename YOURREDISPRIMARYKEY
 ```
 
+> Use a custom port
+
+```powershell
+cachedebugger.exe my-redis-resourcename YOURREDISPRIMARYKEY 6381
+```
+
+> Use a custom endpoint
+
+```powershell
+cachedebugger.exe my-redis-resourcename YOURREDISPRIMARYKEY some.other.redis.endpoint
+```
+
+> Use a custom port and disable SSL (enabled by default)
+
+```powershell
+cachedebugger.exe my-redis-resourcename YOURREDISPRIMARYKEY 6399 false
+```
+
+## TODO
+
+- [ ] Update to support secure strings
 
 ## License
 
--- TestRedisConnection/Program.cs
+++ TestRedisConnection/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using StackExchange.Redis;
@@ -6,74 +9,96 @@ using static System.Text.RegularExpressions.Regex;
 
 namespace TestRedisConnection
 {
-  class Program
-  {
-    #region Properties and Fields
-
-    private static string _ourNode;
-    private static string _primaryAccessKey;
-    private static string _endpointString;
-    private static bool _connectionSucceeded;
-    private static string _endUrl = "redis.cache.windows.net";
-    private static string _port = "6380";
-    private static ConnectionMultiplexer _muxer;
-
-    #endregion
-
-    static async Task Main(string[] args)
+    class Program
     {
-      SetVars(args);
-      await ConnectionMethod1();
-    }
+        #region Properties and Fields
 
-    private static async Task ConnectionMethod1()
-    {
-      var config = new ConfigurationOptions
-      {
-        EndPoints = { _endpointString },
-        Password = _primaryAccessKey,
-        Ssl = true
-      };
-      Console.WriteLine(config.ToString().Replace(_primaryAccessKey, "********"));
-      try
-      {
-        _muxer = await ConnectionMultiplexer.ConnectAsync(config);
-        _connectionSucceeded = _muxer.IsConnected;
-      }
-      catch (Exception)
-      {
-        // We don't do anything with the exception here because we only care if the connection succeeds. 
-        _connectionSucceeded = false;
-      }
+        private static string _ourNode;
+        private static string _primaryAccessKey;
+        private static string _endpointString;
+        private static bool _connectionSucceeded;
+        private static string _endUrl = "redis.cache.windows.net";
+        private static string _port = "6380";
+        private static bool _useSsl = true;
+        private const string _fqdnPattern = @"^([a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9]\.)+[a-zA-Z]{2,}$";
+        private const string _portPattern = @"^\d*$";
+        private const string _boolPattern = @"^true|false$";
 
-      if (_connectionSucceeded)
-      {
-        await _muxer.CloseAsync();
-        _muxer.Dispose();
-      }
+        private static ConnectionMultiplexer _muxer;
 
-      Environment.ExitCode = _connectionSucceeded ? 1 : 0;
-      Console.WriteLine($"Connection Succeeded : {_connectionSucceeded}");
-      Console.WriteLine("Press any key to continue...");
-      Console.ReadLine();
-    }
+        #endregion
 
-    static void SetVars(string[] args)
-    {
-      if (args.Length > 2)
-      {
-        bool arg2IsPort = IsMatch(args[2], $"^\\d*$");
-        if (args.Length == 3 && !arg2IsPort) _endUrl = args[2];
-        if (args.Length == 3 && arg2IsPort)
+        static async Task Main(string[] args)
         {
-          _port = args[2];
+            SetVars(args);
+            await ConnectionMethod1();
         }
-      }
 
-      _ourNode = args[0];
-      _primaryAccessKey = args[1];
-      _endpointString = $"{_ourNode}.{_endUrl}:{_port}";
+        private static async Task ConnectionMethod1()
+        {
+            var config = new ConfigurationOptions
+            {
+                EndPoints = { _endpointString },
+                Password = _primaryAccessKey,
+                Ssl = _useSsl
+            };
+            Console.WriteLine(config.ToString().Replace(_primaryAccessKey, "********"));
+            try
+            {
+                _muxer = await ConnectionMultiplexer.ConnectAsync(config);
+                _connectionSucceeded = _muxer.IsConnected;
+            }
+            catch (Exception)
+            {
+                // We don't do anything with the exception here because we only care if the connection succeeds. 
+                _connectionSucceeded = false;
+            }
+
+            if (_connectionSucceeded)
+            {
+                await _muxer.CloseAsync();
+                _muxer.Dispose();
+            }
+
+            Environment.ExitCode = _connectionSucceeded ? 1 : 0;
+            Console.WriteLine($"Connection Succeeded : {_connectionSucceeded}");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+
+        private static void SetVars(string[] args)
+        {
+            if (args.Length > 2)
+            {
+                foreach (var arg in args.Skip(2))
+                {
+                    bool argIsPort = IsMatch(arg, _portPattern);
+                    bool argIsFqdn = IsMatch(arg, _fqdnPattern);
+                    bool argIsUseSsl = IsMatch(arg, _boolPattern);
+
+                    if (args.Length > 2 && argIsFqdn)
+                    {
+                        Console.WriteLine("Setting url to " + arg);
+                        _endUrl = arg;
+                    }
+
+                    if (args.Length > 2 && argIsPort)
+                    {
+                        Console.WriteLine("Using port 6380");
+                        _port = arg;
+                    }
+
+                    if (args.Length > 2 && argIsUseSsl)
+                    {
+                        Console.WriteLine("Using SSL");
+                        _useSsl = bool.Parse(arg);
+                    }
+                }
+            }
+
+            _ourNode = args[0];
+            _primaryAccessKey = args[1];
+            _endpointString = $"{_ourNode}.{_endUrl}:{_port}";
+        }
     }
-  }
-
 }
